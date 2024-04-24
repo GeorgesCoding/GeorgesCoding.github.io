2c6a8c47a7c7dabe849a7340d67e09acfd73a1eb
GeorgesCoding <chen.george148@gmail.com>
2024-04-24 13:46:06 -0400

    jQuery html page loading test

--- functions.js
+++ functions.js
@@ -18,41 +18,43 @@ function flip(x) {
 }
 
 
-/* Gets the information inputted from the form and sends it using mail.js */
-function send(name, email, message) {
-    var captchaToken = grecaptcha.getResponse();
-
-    if (captchaToken.length == 0) {
-        alert("Please complete the reCAPTCHA before sending your message!")
-    }
-    else {
-        var templateParams = {
-            to_name: 'George',
-            from_name: name,
-            subject: 'Contact Form Message',
-            message: "Email: " + email + "\n\n" + message,
-            "g-recaptcha-response": captchaToken
-        };
-        emailjs.send('service_j2mkwmy', 'template_zilck42', templateParams, 'OmkW-QBfWdiQpxshy').then(
-            (response) => {
-                alert("Message successfully sent!");
-            },
-            (error) => {
-                alert("There was an error in sending the message. Please contact support@georgescoding.com.");
-            },
-        );
-        document.getElementById("form").reset();
-        grecaptcha.reset();
-    }
+function load() {
+    var template = document.getElementById("projectTemplate");
+    var content = template.content;
+    var clone = content.cloneNode(true);
+    document.body.appendChild(clone);
 }
 
 
-/* Validates the contents of the form */
+/* Sends form to email via EmailJS, resets form and reCAPTCHA */
+function send(name, email, message) {
+    var templateParams = {
+        to_name: 'George',
+        from_name: name,
+        subject: 'Contact Form Message',
+        message: "Email: " + email + "\n\n" + message,
+        "g-recaptcha-response": captchaToken
+    };
+    emailjs.send('service_j2mkwmy', 'template_zilck42', templateParams, 'OmkW-QBfWdiQpxshy').then(
+        (response) => {
+            alert("Message successfully sent!");
+        },
+        (error) => {
+            alert("There was an error in sending the message. Please contact support@georgescoding.com.");
+        },
+    );
+    document.getElementById("form").reset();
+    grecaptcha.reset();
+}
+
+
+/* Validates the contents of the form and reCAPTCHA*/
 function valid() {
     var name = String(document.querySelector('[name="name"]').value);
     var email = String(document.querySelector('[name="email"]').value);
     var message = String(document.querySelector('[name="message"]').value);
     var format = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+    var captchaToken = grecaptcha.getResponse();
 
     if (name == "") {
         alert("Name must be filled");
@@ -69,6 +71,9 @@ function valid() {
     else if (!(String(email).match(format))) {
         alert("Email is incorrect");
         document.getElementById("email").focus();
+    }
+    else if (captchaToken.length == 0) {
+        alert("Please complete the reCAPTCHA before sending your message!")
     }
     else { send(name, email, message); }
 }
