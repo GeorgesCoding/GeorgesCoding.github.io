4e5296fc54cb15520736915a21db3a2d8c4e2d2a
GeorgesCoding <chen.george148@gmail.com>
2024-04-24 14:15:22 -0400

    jQuery html page loading test

---
+++ delete.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+
+FILES = ""
+
+""" delete /index.html
+delete file:///C:/Users/cheng/Downloads/Coding/Portfolio"""
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
